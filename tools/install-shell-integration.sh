a41c1719f9e3f278243f3ed0c58bc0373d63ee1c
Sean Sullivan <ssullivan61198@gmail.com>
2021-09-13 16:41:33 -0700

    Add -z flag to tar

--- tools/install-shell-integration.sh
+++ tools/install-shell-integration.sh
@@ -1,6 +1,6 @@
 mkdir -p ~/.fig/
 
-curl -Ls fig.sh/shell-integration.tar.gz | tar -x -C ~/.fig &&
+curl -Ls fig.sh/shell-integration.tar.gz | tar -xz -C ~/.fig &&
   ~/.fig/tools/install_integrations.sh --minimal &&
   echo Successfully installed fig shell integrations ||
   echo Failed to download and extract shell integration
