f23cba8aff139fb21e1930b8db38120afd0edd38
Sean Sullivan <ssullivan61198@gmail.com>
2021-09-13 15:49:51 -0700

    Fix shell integration script

--- tools/install-shell-integration.sh
+++ tools/install-shell-integration.sh
@@ -6,12 +6,10 @@ error() {
 }
 
 cd ~/.fig/
-curl fig.sh/shell-integration.tar.gz | tar -xvf ||
+curl -Ls fig.sh/shell-integration.tar.gz | tar -x ||
   error "Failed to download and extract shell integration"
 
 source ~/.fig/tools/install_utils.sh
 
 append_to_profiles
-install_tmux_integration
-
 echo success
