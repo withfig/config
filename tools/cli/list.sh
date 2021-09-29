7b2559221de4e2650ca496b72a5d9a2c211a09eb
Wes Feller <wes@fig.io>
2021-09-28 17:49:01 -0700

    Get list of specs from remote repo
    Specs are now served from skypack so looking at local specs is outdated

--- tools/cli/list.sh
+++ tools/cli/list.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/env bash
 
-ls ~/.fig/autocomplete | grep -v README | cut -d '.' -f 1
+while IFS= read -ra line; do 
+    IFS=\":. read -ra file <<<"${line[@]}"
+    echo "${file[4]}"
+done <<<"$(curl \
+  -H "Accept: application/vnd.github.v3+json" \
+  https://api.github.com/repos/withfig/autocomplete/contents/src | grep "name")"
